57ba4f4527f513258f91e937a0913c9035f73627
Justin Zhang <justin.z.zhang@gmail.com>
2021-08-04 21:47:22 -0500

    change the redirectURI for Azure cloud deployment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = {
-    appid:'6a442129-c8d8-4b15-bc83-4e38798caf42', //'0b133655-4fa9-4e0a-a9ed-d00e407f463c',
-    redirectUri:'http://localhost:3000', //'https://azfaderation.b2clogin.com/azfaderation.onmicrosoft.com/oauth2/authresp',
+    appid:'6a442129-c8d8-4b15-bc83-4e38798caf42', // azure ad B2C registered application id
+    redirectUri:'https://dhweb.z13.web.core.windows.net', //'http://localhost:3000', 
     scopes: ['https://azfaderation.onmicrosoft.com/crudui/Product.View'],
-    validateAuthority: false,
+    knownAuthorities: ['https://azfaderation.b2clogin.com/azfaderation.onmicrosoft.com/B2C_1_signupandsignin'],
     authority:'https://azfaderation.b2clogin.com/azfaderation.onmicrosoft.com/B2C_1_signupandsignin', 
     google_client_id : '910556168224-5gtfba6aqsb2pssfjq1k1a2gv93bune0.apps.googleusercontent.com',
 };
